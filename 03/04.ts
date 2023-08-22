9084f2c2d9bb14d68aa94553e28a53cb2815b2fa
0gunkim <goldenprevue@gmail.com>
2023-08-22 22:17:32 +0900

    Feat: 03-05~06

---
+++ 03/04.ts
@@ -0,0 +1,21 @@
+class Square {
+  constructor(public width: number) {}
+}
+class Rectangle extends Square {
+  constructor(public width: number, public height: number) {
+    super(width);
+  }
+}
+type Shape = Square | Rectangle;
+
+function calculateArea(shape: Shape) {
+  if (shape instanceof Rectangle) {
+    shape; // Type is Rectangle
+    return shape.width * shape.height;
+  } else {
+    shape; // Type is Square
+    return shape.width * shape.width; // OK
+  }
+}
+// Rectangle 을 클래스로 선언하면 타입과 값으로 모두 사용할 수 있다.
+export default {};
